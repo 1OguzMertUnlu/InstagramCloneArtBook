0fb6ef1d214a0b5000f4ec1a3d6ffa6061729860
1OguzMertUnlu <oguzmertunlu@gmail.com>
2023-04-23 15:48:06 +0300

    InstagramCloneArtBook
    A simple instagram-like art book for android phones
    
    RecyclerView work

---
+++ InstagramCloneJava/app/src/main/java/com/oguzmertunlu/instagramclonejava/model/Post.java
@@ -0,0 +1,14 @@
+package com.oguzmertunlu.instagramclonejava.model;
+
+public class Post {
+
+    public String email;
+    public String comment;
+    public String downloadURL;
+
+    public Post(String email, String comment, String downloadURL) {
+        this.email = email;
+        this.comment = comment;
+        this.downloadURL = downloadURL;
+    }
+}
